63f47f48d8ca04a70b989f74544652f1a1820e05
Wilmer Arambula <terabytesoftw@gmail.com>
2024-03-12 10:58:01 -0300

    Add trait `HasLinkAreaCurrent` class.

---
+++ tests/Component/HasLinkAreaCurrentTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UIAwesome\Html\Concern\Tests\Component;
+
+use UIAwesome\Html\Concern\Component\HasLinkAreaCurrent;
+
+final class HasLinkAreaCurrentTest extends \PHPUnit\Framework\TestCase
+{
+    public function testImmutability(): void
+    {
+        $instance = new class () {
+            use HasLinkAreaCurrent;
+        };
+
+        $this->assertNotSame($instance, $instance->linkAriaCurrent());
+    }
+
+    public function testIsLinkAriaCurrent(): void
+    {
+        $instance = new class () {
+            use HasLinkAreaCurrent;
+        };
+
+        $this->assertFalse($instance->isLinkAriaCurrent());
+        $this->assertTrue($instance->linkAriaCurrent()->isLinkAriaCurrent());
+    }
+}
